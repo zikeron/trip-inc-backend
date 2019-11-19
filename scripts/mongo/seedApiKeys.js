7c0ea7a68a6d543db59e426565092d94c296be37
Alejandro Cortez <ziker@live.com.mx>
2019-11-19 00:24:25 -0600

    Backend for Trip inc

---
+++ scripts/mongo/seedApiKeys.js
@@ -0,0 +1,51 @@
+// DEBUG=app:* node scripts/mongo/seedApiKeys.js
+
+const chalk = require('chalk');
+const crypto = require('crypto');
+const debug = require('debug')('app:scripts:api-keys');
+const MongoLib = require('../../lib/mongo');
+
+const adminScopes = [
+  'signin:auth',
+  'signup:auth',
+];
+
+const publicScopes = [
+  'signin:auth',
+  'signup:auth',
+];
+
+const apiKeys = [
+  {
+    token: generateRandomToken(),
+    scopes: adminScopes
+  },
+  {
+    token: generateRandomToken(),
+    scopes: publicScopes
+  }
+];
+
+function generateRandomToken() {
+  const buffer = crypto.randomBytes(32);
+  return buffer.toString('hex');
+}
+
+async function seedApiKeys() {
+  try {
+    const mongoDB = new MongoLib();
+
+    const promises = apiKeys.map(async apiKey => {
+      await mongoDB.create('api-keys', apiKey);
+    });
+
+    await Promise.all(promises);
+    debug(chalk.green(`${promises.length} api keys have been created succesfully`)); // prettier-ignore
+    return process.exit(0);
+  } catch (error) {
+    debug(chalk.red(error));
+    process.exit(1);
+  }
+}
+
+seedApiKeys();
